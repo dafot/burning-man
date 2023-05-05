bbef9b3e1b0834ac3049206ab37245b6d4aa35d2
Dany Fot <lu4imira@yandex.ru>
2023-05-05 09:54:53 +0300

    adapt header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('hi');
+
+
+'use strict';
+
+const burger = document.querySelector('.burger-menu');
+const nav = document.querySelector('.header__nav')
+console.log(nav);
+
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+})
